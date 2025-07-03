97e16e15706cdfe438e1ecad6be46648edce69f9
Matthias Bauer <mbauer@student.42heilbronn.de>
2025-07-03 14:03:33 +0200

    finished ft_tolower and ft_toupper and commented out main

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: mbauer <mbauer@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 12:50:36 by mbauer            #+#    #+#             */
-/*   Updated: 2025/07/03 13:14:28 by mbauer           ###   ########.fr       */
+/*   Updated: 2025/07/03 14:03:19 by mbauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,29 +20,29 @@ int	ft_isdigit(int c)
 	return (0);
 }
 
-int	main(void)
-{
-	// char test_cha[] = {'0', '5', '9', 'a', '!', '3', '7', 'Z', '\0'};
-	int test_chars[] = {0, 5, 9, 'a', '!', 3, 7, 'Z', '\0'};
-	// char test_chars[] = {'\t', '5', '9', 'a', '!', 3, '7', 'Z', '\0'};
-	int i = 0;
+// int	main(void)
+// {
+// 	// char test_cha[] = {'0', '5', '9', 'a', '!', '3', '7', 'Z', '\0'};
+// 	int test_chars[] = {0, 5, 9, 'a', '!', 3, 7, 'Z', '\0'};
+// 	// char test_chars[] = {'\t', '5', '9', 'a', '!', 3, '7', 'Z', '\0'};
+// 	int i = 0;
 
-	while (i < 9)
-	{
-		if (ft_isdigit(test_chars[i]))
-			printf("'%c' is a digit. %d\n", test_chars[i],
-				ft_isdigit(test_chars[i]));
-		else
-			printf("'%c' is not a digit. %d\n", test_chars[i],
-				ft_isdigit(test_chars[i]));
-		if (isdigit(test_chars[i]))
-			printf("'%c' is a digit original. %d\n",
-				test_chars[i], isalpha(test_chars[i]));
-		else
-			printf("'%c' is not a digit original %d.\n",
-				test_chars[i], isalpha(test_chars[i]));
-		i++;
-		i++;
-	}
-	return (0);
-}
+// 	while (i < 9)
+// 	{
+// 		if (ft_isdigit(test_chars[i]))
+// 			printf("'%c' is a digit. %d\n", test_chars[i],
+// 				ft_isdigit(test_chars[i]));
+// 		else
+// 			printf("'%c' is not a digit. %d\n", test_chars[i],
+// 				ft_isdigit(test_chars[i]));
+// 		if (isdigit(test_chars[i]))
+// 			printf("'%c' is a digit original. %d\n",
+// 				test_chars[i], isalpha(test_chars[i]));
+// 		else
+// 			printf("'%c' is not a digit original %d.\n",
+// 				test_chars[i], isalpha(test_chars[i]));
+// 		i++;
+// 		i++;
+// 	}
+// 	return (0);
+// }
