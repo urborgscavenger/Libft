a548dc8c154279075b682291943f089ea49d5da5
Matthias Bauer <mbauer@student.42heilbronn.de>
2025-07-03 15:00:18 +0200

    finished ft_strlen recursively

---
+++ ft_strlen.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbauer <mbauer@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/03 14:46:23 by mbauer            #+#    #+#             */
+/*   Updated: 2025/07/03 14:51:56 by mbauer           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_len_recursiv(const char *s, int i)
+{
+	if (s[i] == '\0')
+		return (i);
+	return (ft_len_recursiv(s, i + 1));
+}
+
+int	ft_strlen(const char *s)
+{
+	int i;
+
+	i = 0;
+	if (s[i] != '\0')
+		return (ft_len_recursiv(s, i));
+	return (i);
+}
+
+// #include <string.h>
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	const char *test_str = "0";
+// 	int length;
+// 	int length_og;
+
+// 	length = ft_strlen(test_str);
+// 	length_og = strlen(test_str);
+// 	printf("The length of the string '%s' is: %d\n", test_str, length);
+// 	printf("The length of the string '%s' is: %d (OG)\n", test_str, length_og);
+// 	return (0);
+// }
