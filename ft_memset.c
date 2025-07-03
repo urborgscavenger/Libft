245759aa54b3d1d1926f8d31512b6bf5a2b70045
Matthias Bauer <mbauer@student.42heilbronn.de>
2025-07-03 15:14:26 +0200

    finished ft_memset recursively

---
+++ ft_memset.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbauer <mbauer@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/03 15:00:59 by mbauer            #+#    #+#             */
+/*   Updated: 2025/07/03 15:14:07 by mbauer           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdio.h>
+
+void	mset_recursiv(void *dist, int c, size_t n, size_t i)
+{
+	if (i < n)
+	{
+		((unsigned char *)dist)[i] = (unsigned char)c;
+		mset_recursiv(dist, c, n, i + 1);
+	}
+}
+
+void	*ft_memset(void *dist, int c, size_t n)
+{
+	unsigned char	*ptr;
+	size_t			i;
+	ptr = (unsigned char *)dist;
+	i = 0;
+	mset_recursiv(ptr, c, n, i);
+	return (dist);
+}
+
+int	main()
+{
+	char str[50] = "";
+	char str2[50] = "";
+	printf("Before memset: %s\n", str);
+	printf("Before OGmemset: %s\n", str2);
+	ft_memset(str, 'X', 5);
+	memset(str2, 'X', 5);
+	printf("After memset: %s\n", str);
+	printf("After OGmemset: %s\n", str2);
+	return 0;
+}
